e10c2deb01f093e19b81ad38da6df9857c9ae95a
vagrant <vagrant@precise32.(none)>
2014-02-28 22:31:19 +0000

    first commit analytics lab

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,14 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
-exports.view = function(req, res){
+exports.view = function(req, res){	
+	projects['grid'] = false;
+	console.log(projects);
+  	res.render('index', projects);
+};
+
+exports.viewGrid= function(req, res){
+	projects['grid'] = true;
+	console.log(projects);
   	res.render('index', projects);
 };
